588994287fb4da332852f76eef8f7299fa8ae050
rishikJain <49618560+rishikJain@users.noreply.github.com>
2022-04-01 10:38:23 +0530

    Added test2

---
+++ shm/test2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define key 0x1234
+#define PAGE_SIZE (1 << 12)
+
+int main(){
+    int fd = open("test_file.txt", O_RDWR);
+    int shm_id = shmget(key, PAGE_SIZE, IPC_CREAT | 0600);
+    void* mem = shmat(shm_id, NULL, 0);
+    // void* mem = shmat(shm_id, NULL, SHM_RDONLY);
+    mmap(mem, PAGE_SIZE*4, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_FIXED, fd, 0);
+    // *(int*)mem = 'R'+'P'+'C';
+    shmdt(mem);
+    shmctl(shm_id, IPC_RMID, NULL);
+    return 0;
+}
